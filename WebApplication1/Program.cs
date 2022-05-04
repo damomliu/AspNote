989a9e676a63041c09b45b38f401fe84eef49c41
damon.122 <damomliu@gmail.com>
2022-05-04 15:15:00 +0800

    增加 controller (WebAPI 範本)

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+
+// 重導向至 Https
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
